055a8913c717b9fdce0ab4f0d593d9d76cb8411a
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-02-16 14:11:32 +0530

    Change1:Print in Bash

---
+++ shell-scripting.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# We can print a message on screen using two commands in linux
+# 1. echo   -> Preferred one , because of less syntax
+# 2. printf  -> Have more syntax
+
+echo hello world
+echo "hello world"
+echo -e "Hello\n\n Welcome to Shell-Scriping\n\nRegards\nSrimaan"
