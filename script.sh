b799fd023e9171b0750bf6346f4a50d13b2d681a
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-02-17 09:32:29 +0530

    Change1 : Added Case Conditioin

--- script.sh
+++ script.sh
@@ -1,3 +1,13 @@
 echo "Script Name:"$0
 echo -e "First Name:\e[34m$1\e[0m"
 echo -e "Last Name:\e[35m$2\e[0m"
+
+case $1 in
+    frontend)
+      echo "Installing Frontend"
+      ;;
+    mongo)
+      echo "Installing Mongo"
+      ;;
+
+esac
