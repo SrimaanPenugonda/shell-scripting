75443d78c3008c31769f2e99a3174d47182fc6c4
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-02-20 12:57:26 +0530

    Change1:Added Frontend

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 COMPONENT=frontend
-echo -e "[\e[1;33mINFO\e[0m] [\e[1;34m${COMPONENT}\e[0m] [\e[1;35m$(date +%D %T)\e[0m]"
+echo -e "[\e[1;33mINFO\e[0m] [\e[1;34m${COMPONENT}\e[0m] [\e[1;35m$(date +%F:%T)\e[0m]"
