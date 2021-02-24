c72394454ef2320903f8e178f156c0fb26c4a3ac
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-02-24 21:10:36 +0530

    Change1:redis setup

---
+++ roboshop/components/redis.sh
@@ -0,0 +1,28 @@
+#!/bin/bash/
+
+COMPONENT=redis
+
+source components/common.sh
+
+INFO "Setup Redis Component"
+
+INFO "Setup REDIS repos"
+yum install epel-release yum-utils -y &>>$Log_File
+yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$Log_File
+yum-config-manager --enable remi &>>$Log_File
+INFO "Install Redis"
+yum install redis -y &>>$Log_File
+STAT $? "Redis Installation"
+
+INFO "Update Redis Configuration redis.conf"
+if [ -f /etc/redis.conf ]; then
+  sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/redis.conf &>>$Log_File
+  STAT $? "redis conf file update"
+else
+  STAT 1 "redis.conf file missing"
+fi
+
+INFO "Start Redis"
+systemctl enable redis &>>$Log_File
+systemctl restart redis &>>$Log_File
+STAT $? "Redis Service Startup"
