eb0ba0d89e707f34b73eb96178c86c268c8bd5f6
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-02-25 08:13:32 +0530

    Change1:mysql setup

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -10,7 +10,7 @@ echo '[mysql57-community]
 name=MySQL 5.7 Community Server
 baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
 enabled=1
-gpgcheck=0' > /etc/yum.repos.d/mysql.repo &>>$Log_File
+gpgcheck=0' > /etc/yum.repos.d/mysql.repo
 STAT $? "Mysql yum repository"
 
 INFO " Install Mysql"
