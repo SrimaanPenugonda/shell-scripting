8081af0a18a939ed13abd8436f6dd05d45ad2e46
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-03-05 20:39:12 +0530

    Change1:correction

--- roboshop/launch-instances.sh
+++ roboshop/launch-instances.sh
@@ -14,7 +14,8 @@ case $1 in
         echo "updating routes"
         for component in frontend catalogue cart user shipping payment mysql mongo rabbitmq redis;do
           echo "create A record for ${component}"
-          IP=$(aws ec2 describe-instances --filter Name=tag:Name,Values=${component} Name=instance-state-name,Values=running | jq '.Reservations[].Instances[].PrivateIpAddress')
+          IP=$(aws ec2 describe-instances --filter Name=tag:Name,Values=${component} Name=instance-state-name,Values=running | jq \
+          '.Reservations[].Instances[].PrivateIpAddress')
           sed -e "s/COMPONENT/${component}/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/${component}.json
           aws route53 change-resource-record-sets --hosted-zone-id Z054832619MOTOCO0ITHS --change-batch file:///tmp/${component}.json
           done
