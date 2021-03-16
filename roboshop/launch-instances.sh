20b1e4c7d972146be12c574a9a43dcac5530cfe0
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-03-16 09:53:56 +0530

    Change1:updated new hostedzone

--- roboshop/launch-instances.sh
+++ roboshop/launch-instances.sh
@@ -18,7 +18,7 @@ case $1 in
           IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${component} Name=instance-state-name,Values=running | jq \
           '.Reservations[].Instances[].PrivateIpAddress')
           sed -e "s/COMPONENT/${component}/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/${component}.json
-          aws route53 change-resource-record-sets --hosted-zone-id Z054832619MOTOCO0ITHS --change-batch file:///tmp/${component}.json
+          aws route53 change-resource-record-sets --hosted-zone-id Z02790743IF4OLO4KMMFA --change-batch file:///tmp/${component}.json
           done
         ;;
       terminate)
