1609a12d6c4a85b0afe4638996d9b6d1d28c5062
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-03-17 18:08:03 +0530

    Change1:terraform variables

--- terraform/input/input.tf
+++ terraform/input/input.tf
@@ -1,9 +1,10 @@
 //to declare a variable
-variable "abc" {} // empty variable declaration with out default value
+variable "abc" {
+  type = number  // it expects number only
+} // empty variable declaration with out any default value , mention the datatype 'type'
 
 //to access this variable
 
 output "abc" {
-  type = number  // it expects number only
   value = var.abc
 } // this will ask for value after terraform apply command
