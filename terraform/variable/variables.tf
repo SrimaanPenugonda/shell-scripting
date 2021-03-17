3681e4e03728e44174231cccc60e0a140a6dc8b0
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-03-17 17:11:18 +0530

    Change1:terraform variables

--- terraform/variable/variables.tf
+++ terraform/variable/variables.tf
@@ -6,4 +6,20 @@ output "a" {
   value = var.a
 } //access a variable var.a
 
+// types of variables
+//1.normal
 
+variable "b" {
+  default = 200
+}
+//2.list
+variable "list"{
+  default = [100,"abcd",true]
+} // list will have diff data types
+
+output "list_1" {
+  value = var.list[1]
+}
+output "list_2" {
+  value = var.list[2]
+}
