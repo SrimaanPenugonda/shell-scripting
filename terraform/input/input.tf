5171d09634a9175d4f33fea968a1365797d1ed78
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-03-17 18:35:22 +0530

    Change1:terraform variables

--- terraform/input/input.tf
+++ terraform/input/input.tf
@@ -3,7 +3,7 @@ variable "abc" {
   type = number  // it expects number only
 } // empty variable declaration with out any default value , mention the datatype 'type'
 
-//to access this variable over CLI
+//1.to access this variable over CLI
 output "abc" {
   value = var.abc
 } // this will ask for value after terraform apply command,
@@ -12,9 +12,14 @@ variable "D" {} // by default it takes as string
 output "D" {
   value = var.D
 }
-// to pass as argument
+//2.to pass as argument
 //terraform -auto-approve -var abc1=data
 variable "abc1" {}
 output "abc1" {
   value = var.abc1
+}
+//3.pass variable from file
+variable "abc2" {}
+output "abc2" {
+  value = var.abc2
 }
