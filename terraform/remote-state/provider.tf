2bbcb67004c733410d787255e2042ea906713533
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-03-18 19:20:52 +0530

    Change1:upload state file to s3 bucket

--- terraform/remote-state/provider.tf
+++ terraform/remote-state/provider.tf
@@ -3,4 +3,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
+//upload state file to s3 bucket
+terraform {
+  backend "s3" {
+    bucket = "srimaan-s3-2" //bucketname
+    key    = "sample/terraform.tfstate" //path in bucket
+    region = "us-east-1"
+  }
+}
