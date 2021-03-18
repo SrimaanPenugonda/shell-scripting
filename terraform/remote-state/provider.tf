8dc5fc082a699e47610f7a41e9964be3eaa8546a
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-03-18 18:53:40 +0530

    Change1:Notes

--- terraform/remote-state/provider.tf
+++ terraform/remote-state/provider.tf
@@ -2,15 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-//create bucket
-resource "aws_s3_bucket" "b" {
-  bucket = "srimaan_s3_1"
-  acl = "private"
 
-  versioning {
-    enabled = true
-  }
-}
 //upload state file to s3 bucket
 terraform {
   backend "s3" {
