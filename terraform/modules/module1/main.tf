0b5fa3b9696ca117572ae00b2cf48e7304cb6b7d
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-03-20 09:46:52 +0530

    Change1:

--- terraform/modules/module1/main.tf
+++ terraform/modules/module1/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "sample" {
   ami           = "ami-052ed3344670027b3" // AMI Devops-Practice
   instance_type = "t2.micro"
 }
-resource "aws_instance" "sample" {
+resource "aws_instance" "sample1" {
   ami           = "ami-052ed3344670027b3" // AMI Devops-Practice
   instance_type = var.INSTANCE_TYPE //use the variable
 }
