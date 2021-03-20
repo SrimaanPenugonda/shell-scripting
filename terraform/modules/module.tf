b195b38090b06d3b079862d0902dd1e877d92344
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-03-20 09:34:01 +0530

    Change1:

--- terraform/modules/module.tf
+++ terraform/modules/module.tf
@@ -2,6 +2,10 @@ module "module1" {
   source = "./module1"
 } //calling the module1
 
+module "module2" {
+  source = "./module2"
+} //calling module2
+
 provider "aws" {
   region = "us-east-1"
 }
---
+++ terraform/modules/module2/main.tf
@@ -0,0 +1,6 @@
+//resources..
+//        resource name  local name
+resource "aws_instance" "sample" {
+  ami           = "ami-052ed3344670027b3" // AMI Devops-Practice
+  instance_type = "t3.micro"
+}
