1e98929a8b06f823e9f8aa8a9a96ebefeec9f125
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-03-23 00:25:33 +0530

    Change1:

--- terraform-vpc/vpc.tf
+++ terraform-vpc/vpc.tf
@@ -7,3 +7,25 @@ resource "aws_vpc" "main" {
     Provisioned         = "Terraform"
   }
 }
+
+resource "aws_subnet" "private-subnets" {
+  count       = 2
+  cidr_block  = cidrsubnet(var.VPC_CIDR,2,count.index)
+  vpc_id      = aws_vpc.main.id
+  tags                  = {
+    Name                = "${var.PROJECT_NAME}-${var.ENV}-private-subnet-${count.index+1}"
+    Environment         = var.ENV
+    Provisioned         = "Terraform"
+  }
+}
+
+resource "aws_subnet" "public-subnets" {
+  count       = 2
+  cidr_block  = cidrsubnet(var.VPC_CIDR,2,count.index+2)
+  vpc_id      = aws_vpc.main.id
+  tags                  = {
+    Name                = "${var.PROJECT_NAME}-${var.ENV}-public-subnet-${count.index+1}"
+    Environment         = var.ENV
+    Provisioned         = "Terraform"
+  }
+}
